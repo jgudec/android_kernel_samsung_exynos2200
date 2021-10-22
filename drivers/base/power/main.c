ab63e2f8c54c5ce4766bf1f3b686f70fc89b255e
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2021-10-22 14:58:23 +0200

    PM: sleep: Do not let "syscore" devices runtime-suspend during system transitions
    commit 928265e3601cde78c7e0a3e518a93b27defed3b1 upstream.
    
    There is no reason to allow "syscore" devices to runtime-suspend
    during system-wide PM transitions, because they are subject to the
    same possible failure modes as any other devices in that respect.
    
    Accordingly, change device_prepare() and device_complete() to call
    pm_runtime_get_noresume() and pm_runtime_put(), respectively, for
    "syscore" devices too.
    
    Fixes: 057d51a1268f ("Merge branch 'pm-sleep'")
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Cc: 3.10+ <stable@vger.kernel.org> # 3.10+
    Reviewed-by: Ulf Hansson <ulf.hansson@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/base/power/main.c
+++ drivers/base/power/main.c
@@ -1065,7 +1065,7 @@ static void device_complete(struct device *dev, pm_message_t state)
 	const char *info = NULL;
 
 	if (dev->power.syscore)
-		return;
+		goto out;
 
 	device_lock(dev);
 
@@ -1095,6 +1095,7 @@ static void device_complete(struct device *dev, pm_message_t state)
 
 	device_unlock(dev);
 
+out:
 	pm_runtime_put(dev);
 }
 
@@ -1823,9 +1824,6 @@ static int device_prepare(struct device *dev, pm_message_t state)
 	int (*callback)(struct device *) = NULL;
 	int ret = 0;
 
-	if (dev->power.syscore)
-		return 0;
-
 	/*
 	 * If a device's parent goes into runtime suspend at the wrong time,
 	 * it won't be possible to resume the device.  To prevent this we
@@ -1833,6 +1831,9 @@ static int device_prepare(struct device *dev, pm_message_t state)
 	 * it again during the complete phase.
 	 */
 	pm_runtime_get_noresume(dev);
+
+	if (dev->power.syscore)
+		return 0;
 
 	device_lock(dev);
 
