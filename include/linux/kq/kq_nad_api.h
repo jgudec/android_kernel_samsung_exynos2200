cae4acc1a14943e223c6bf9c89c0737eb3f281a7
Mohammad Afaneh <mohammad.afaneh92@gmail.com>
2022-05-05 17:37:49 +0300

    Import S906BXXU2AVEH from SM-S906B_12_Opensource.zip

--- include/linux/kq/kq_nad_api.h
+++ include/linux/kq/kq_nad_api.h
@@ -11,6 +11,21 @@ struct kq_nad_api {
 };
 
 static struct kq_nad_api kq_nad_api_pin_list[] = {
+	{ "MAINCAM_SCL_1P8" },
+	{ "MAINCAM_SDA_1P8" },
+	{ "CAM_AF_EEP_SCL_1P8" },
+	{ "CAM_AF_EEP_SDA_1P8" },
+
+	{ "CAM_OIS_SCL_1P8" },
+	{ "CAM_OIS_SDA_1P8" },
+
+	{ "TELECAM_AF_EEP_SCL_1P8" },
+	{ "TELECAM_AF_EEP_SDA_1P8" },
+	{ "UWCAM_SCL_1P8" },
+	{ "UWCAM_SDA_1P8" },
+	{ "TELE_SCL_1P8" },
+	{ "TELE_SDA_1P8" },
+
 	{ "WT3X_OIS_T10X_AF_OIS_SCL_1P8" },
 	{ "WT3X_OIS_T10X_AF_OIS_SDA_1P8" },
 
@@ -24,8 +39,13 @@ static struct kq_nad_api kq_nad_api_pin_list[] = {
 	{ "TELE3X_AF_EEP_SDA_1P8" },
 	{ "VTCAM_SCL_1P8" },
 	{ "VTCAM_SDA_1P8" },
+
+	{ "VTCAM_SCL_1P8" },
+	{ "VTCAM_SDA_1P8" },
 	{ "UWCAM_SCL_1P8" },
 	{ "UWCAM_SDA_1P8" },
+	{ "VTCAM_AF_SCL_1P8" },
+	{ "VTCAM_AF_SDA_1P8" },
 	{ "UW_AF_EEP_SCL_1P8" },
 	{ "UW_AF_EEP_SDA_1P8" },
 
