cbf106923928fcc3a45cc525c4e07cbaafd2c5c0
jgudec <jakov.gudec@gmail.com>
2022-09-05 10:40:05 +0200

    wl_blocker: Update default wakelock list

--- drivers/base/power/boeffla_wl_blocker.h
+++ drivers/base/power/boeffla_wl_blocker.h
@@ -16,7 +16,7 @@
 
 #define BOEFFLA_WL_BLOCKER_VERSION	"1.1.0"
 
-#define LIST_WL_DEFAULT				"wlan_wake;wlan_rx_wake;wlan_ctrl_wake;wlan_txfl_wake;bluetooth_timer;BT_bt_wake;BT_host_wake;bbd_wake_lock;ssp_sensorhub_wake_lock;ssp_wake_lock;ssp_comm_wake_lock;abox;qcom_rx_wakelock;mmc0_detect;grip_wake_lock;wlan_scan_wake;wlan_pm_wake;nfc_wake_lock;umts_ipc0;umts_ipc0;umts_ipc1;19f00000.drmdecon;19eb0000.drmdsim;pmo_wow_wl;sec-battery-vbus;sec-battery-monitor;qcom,cnss-qca6490;"
+#define LIST_WL_DEFAULT				"wlan_wake;wlan_rx_wake;wlan_ctrl_wake;wlan_txfl_wake;bluetooth_timer;BT_bt_wake;BT_host_wake;bbd_wake_lock;ssp_sensorhub_wake_lock;ssp_wake_lock;ssp_comm_wake_lock;abox;qcom_rx_wakelock;mmc0_detect;grip_wake_lock;wlan_scan_wake;wlan_pm_wake;nfc_wake_lock;umts_ipc0;umts_ipc1;umts_rfs0;19f00000.drmdecon;19eb0000.drmdsim;pmo_wow_wl;sec-battery-vbus;sec-battery-monitor;qcom,cnss-qca6490;"
 
 #define LENGTH_LIST_WL				8192
 #define LENGTH_LIST_WL_DEFAULT		(sizeof(LIST_WL_DEFAULT))
