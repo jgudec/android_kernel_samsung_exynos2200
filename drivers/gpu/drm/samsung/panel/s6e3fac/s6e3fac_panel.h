246023c38b9d95b52cefd1c342bb67fd67442f50
Mohammad Afaneh <mohammad.afaneh92@gmail.com>
2022-11-12 19:27:42 +0300

    Import S901BXXU2BVJA from SM-S901B_13_Opensource.zip

--- drivers/gpu/drm/samsung/panel/s6e3fac/s6e3fac_panel.h
+++ drivers/gpu/drm/samsung/panel/s6e3fac/s6e3fac_panel.h
@@ -1,4 +1,8 @@
 #ifndef __S6E3FAC_PANEL_H__
 #define __S6E3FAC_PANEL_H__
+#if IS_ENABLED(CONFIG_SEC_G0S_PROJECT)
 #include "s6e3fac_rainbow_g0_panel.h"
+#elif IS_ENABLED(CONFIG_SEC_R0S_PROJECT)
+#include "s6e3fac_rainbow_r0_panel.h"
+#endif
 #endif /* __S6E3FAC_PANEL_H__ */
